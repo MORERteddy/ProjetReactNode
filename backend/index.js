b054aa677096cb2d1165cf1086ab9aad20b4271f
MORERteddy <morerted@hotmail.com>
2020-12-21 18:44:37 +0100

    route serveur

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.CONNECTION_URI, optionMangoos)
 const app=express()
 
 app.use(express.json())
-app.get("/",async(req,res)=>{
+app.get("/Post",async(req,res)=>{
 	await Post.find()
 		.exec()
 		.then(document => res.status(200).json(document))
@@ -22,6 +22,38 @@ app.post("/Post",async(req,res)=>{
 	res.status(201).json(document)
 })
 
+app.get("/Post/:id",async(req,res)=>{
+    await Post.findById(req.params.id)
+    .exec()
+    .then(document => res.status(200).json(document))
+    .catch(err => res.status(500).send())
+})
+
+app.patch("/Post/:id", async (req, res) => {
+	try {
+		const { id } = req.params
+		const acceptedField = ["voteCount", "comments", "noteMoyenne","titre","body","image"]
+		const keys = Object.keys(req.body).filter(key => acceptedField.includes(key))
+		const fieldsToUpdate = {}
+		keys.map(key => fieldsToUpdate[key] = req.body[key])
+		const doc = await Post.findByIdAndUpdate(id, fieldsToUpdate, {new: true})
+		res.json(doc)
+	} catch {
+		res.status(500).send("Erreur lors de la mise à jour")
+	}
+})
+app.get("/User",async(req,res)=>{
+	await User.find()
+		.exec()
+		.then(document => res.status(200).json(document))
+		.catch(err => res.status(500).send())
+})
+
+app.post("/User",async(req,res)=>{
+    const newUser = new User(req.body)
+	const document = await newUser.save()
+	res.status(201).json(document)
+})
 
 
 app.listen(process.env.PORT,()=>{console.log(`serveur lancé http://localhost:${process.env.PORT}`)})
