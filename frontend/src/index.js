0839e81d797c359ba72ffc94f6b74d91d596500f
MORERteddy <morerted@hotmail.com>
2020-12-22 10:54:53 +0100

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,9 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+
        <App />     
-    </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {useState,useEffect} from "react"
 
 function Home() {
+    const[posts,setPosts]=useState([])
+  
+    const getData = () => {
+      fetch("http://localhost:4000/posts")
+        .then(res => res.json())
+        .then(data => { console.log(data)
+          setPosts(data)})}
+          useEffect(() => {
+            getData()
+            console.log(posts)    
+          }, [])
     return (
         <div>
-            
+                  {posts.map((post,index)=>{return <div key={index}>{post._id} {post.titre}</div>})}
         </div>
     )
 }
